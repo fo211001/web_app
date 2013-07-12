5e0f83c74b7d9bf5c72ea73782c0f1d0e6db14e5
Никита <nikitarodionovasfrid@yandex.ru>
2013-07-12 19:05:00 +0600

    2q3123

--- web_app/song/fingering/filters.py
+++ web_app/song/fingering/filters.py
@@ -11,8 +11,8 @@ class AppFilter(object):
 class DistFilter(AppFilter):
     max_dist = None
 
-    def __init__(self, dist):
-        self.max_dist = dist
+    def __init__(self, dist=4):
+        self.max_dist = int(dist)
 
     def filter(self, fingering):
         is_good = True
@@ -26,26 +26,29 @@ class DistFilter(AppFilter):
 
 class OnlyBarreFilter(AppFilter):
     notes = []
+    line_up = ["E", "H", "G", "D", "A", "E"]
 
-    def __init__(self, notes):
+    def __init__(self, notes, count_fingers=4):
         self.notes = notes
+        self.count_fingers = count_fingers
 
     def filter(self, fingering):
         is_barre = False
         probably_indexes_barre = []
-        for i, c in enumerate(fingering):
-            for j in xrange(i+1, len(fingering)):
-                if c == fingering[j]:
-                    probably_indexes_barre.append(c)
-        for index_barre in probably_indexes_barre:
-            if index_barre == min(*fingering):
-                is_barre = True
+        if min(*fingering) != 0:
+            for i, c in enumerate(fingering):
+                for j in xrange(i+1, len(fingering)):
+                    if c == fingering[j]:
+                        probably_indexes_barre.append(c)
+            for index_barre in probably_indexes_barre:
+                if index_barre == min(*fingering):
+                    is_barre = True
         if is_barre:
-            count = 0
-            for i in fingering:
-                if i != min(*fingering) and i != 'x':
-                    count += 1
-            if count != len(self.notes):
+            count_fingers = 0
+            for f in fingering:
+                if f != min(fingering) and f != 'x':
+                    count_fingers += 1
+            if count_fingers >= self.count_fingers:
                 is_barre = False
         return is_barre
 
@@ -65,28 +68,15 @@ class WithoutBarreFilter(AppFilter):
             return False
 
 
-class OnlyAllCordsFilter(AppFilter):
+class WithCordsFilter(AppFilter):
+    def __init__(self, cords=None):
+        self.with_cords = set(cords or [])
 
     def filter(self, fingering):
-        for chord in fingering:
-            if chord == 'x':
+        for i, chord in enumerate(fingering):
+            if i in self.with_cords and chord == 'x':
                 return False
         return True
-
-
-class WithoutCordsFilter(AppFilter):
-    without_cords = []
-
-    def __init__(self, cords):
-        self.without_cords = cords
-
-    def filter(self, fingering):
-        is_good = True
-        for i, chord in enumerate(fingering):
-            if i in self.without_cords:
-                if not chord == 'x':
-                    is_good = False
-        return is_good
 
 
 class AllNotesNeededFilter(AppFilter):
@@ -102,17 +92,28 @@ class AllNotesNeededFilter(AppFilter):
 
 
 class CountOfFingersFilter(AppFilter):
-
     def __init__(self, max_count_of_fingers=4):
-        self.max_count_of_fingers = max_count_of_fingers
+        self.max_count_of_fingers = int(max_count_of_fingers)
 
     def filter(self, fingering):
         count = 0
         for chord in fingering:
-            if chord != 'x':
+            if chord != 'x' and chord != 0:
                 count += 1
         if count <= self.max_count_of_fingers:
             return True
         else:
             return False
+
+
+class TunesFilter(AppFilter):
+    def __init__(self, min_tune=0, max_tune=14):
+        self.min_tune = int(min_tune)
+        self.max_tune = int(max_tune)
+
+    def filter(self, fingering):
+        for chord in fingering:
+            if chord < self.min_tune or chord > self.max_tune:
+                return False
+        return True
 
--- web_app/views.py
+++ web_app/views.py
@@ -1,19 +1,28 @@
 #coding: utf-8
 from pyramid.httpexceptions import HTTPFound, HTTPForbidden
 from pyramid.request import Request
-from pyramid.response import Response
+from pyramid.response import Response, FileResponse
 from pyramid.security import remember, authenticated_userid, forget
 from pyramid.view import view_config, forbidden_view_config
 from sqlalchemy.exc import DBAPIError
 from .models import (
     DBSession,
-    MyModel,
+    User,
+    WebSong,
+    login,
+    register
 )
-from web_app.user import login, register
 
+from song.chord import all_chord_tones
 from song.parse import parse_text
-from user import User, WebSong
 from song.text import song_text
+from song.chord import musicals
+from song.fingering.filters import (AllNotesNeededFilter, CountOfFingersFilter,
+                                    DistFilter, OnlyBarreFilter, TunesFilter, WithCordsFilter, WithoutBarreFilter)
+from song.fingering import iterate_fingerings
+from song.drawer import image_fingering
+
+
 
 
 @forbidden_view_config()
@@ -36,16 +45,6 @@ def auth_required(func):
     return wrapper
 
 
-@view_config(route_name='home', renderer='templates/mytemplate.jinja2')
-@auth_required
-def my_view(request):
-    try:
-        one = DBSession.query(MyModel).filter(MyModel.name == 'one').first()
-    except DBAPIError:
-        return Response(conn_err_msg, content_type='text/plain', status_int=500)
-    return {'one': one, 'project': 'web_app', 'login': True}
-
-
 @view_config(route_name='about', renderer='templates/about.jinja2')
 def about_view(request):
     return {'About': "us"}
@@ -57,9 +56,11 @@ def chords_view(request):
 
 
 def get_current_user(request):
-    id = authenticated_userid(request)
+    id_= authenticated_userid(request)
+    # import pdb; pdb.set_trace()
     session = DBSession()
-    return session.query(User).filter(User.id == id)
+
+    return session.query(User).get(id_)
 
 
 @view_config(route_name='login', renderer='templates/login.jinja2')
@@ -68,8 +69,9 @@ def login_view(request):
     did_fail = False
     if 'email' in request.POST:
         #LOGIN PROCESSING
-        if login(request.POST["email"], request.POST["password"]):
-            headers = remember(request, id)
+        user = login(request.POST["email"], request.POST["password"])
+        if user:
+            headers = remember(request, user.id)
             return HTTPFound(location=nxt, headers=headers)
         else:
             did_fail = True
@@ -98,9 +100,9 @@ def registration_view(request):
     nxt = request.params.get('next') or request.route_url('home')
     did_fail = False
     if 'email' in request.POST:
-        #LOGIN PROCESSING
-        if register(request.POST["name"], request.POST["email"], request.POST["password"]):
-            headers = remember(request, id)
+        user = register(request.POST["name"], request.POST["email"], request.POST["password"])
+        if user:
+            headers = remember(request, user.id)
             return HTTPFound(location=nxt, headers=headers)
         else:
             did_fail = True
@@ -120,17 +122,99 @@ def add_view(request):
         user = get_current_user(request)
         user.songs.append(web_song)
         DBSession().commit()
-        return {'song': song_text(song, song.base_chord)}
+        return HTTPFound(location='/edit/{}'.format(web_song.id))
     return {}
 
 
-@view_config(route_name='songs', renderer='templates/songs.jinja2')
+@view_config(route_name='edit', renderer='templates/edit.jinja2')
+@auth_required
+def edit_view(request):
+    try:
+        song_id = request.matchdict['song_id']
+        web_song = DBSession().query(WebSong).get(song_id)
+        if "song_text" in request.POST:
+            song = parse_text(request.POST["song_text"])
+            song.base_chord = request.POST["base_chord"]
+            web_song.song = song
+            web_song.title = request.POST["title"]
+            # import pdb; pdb.set_trace()
+            if authenticated_userid(request) != web_song.user_id:
+                raise HTTPForbidden()
+            DBSession().commit()
+        if "delete_song" in request.POST:
+            song_delete(song_id)
+            return HTTPFound(location='/')
+        return {'song_text': song_text(web_song.song, web_song.song.base_chord), 'song_title': web_song.title, 'tones':all_chord_tones, 'base_chord': web_song.song.base_chord}
+    except:
+        return HTTPFound(location='/')
+
+@view_config(route_name='home', renderer='templates/songs.jinja2')
+@auth_required
 def songs(request):
     user = get_current_user(request)
-    user_songs_titles = []
-    for songs in user.song:
-        user_songs_titles.append(songs.title)
-    return {'songs': user_songs_titles}
+    return {'songs': user.songs, 'login': True}
+
+
+def song_delete(song_id):
+    web_song = DBSession().query(WebSong).get(song_id)
+    DBSession().delete(web_song)
+    DBSession().commit()
+
+@view_config(route_name='fingering')
+def generate_fingering(request):
+    fingering = request.matchdict['name'].split(',')
+    return FileResponse(image_fingering(fingering))
+
+@view_config(route_name='chord', renderer='templates/chord.jinja2')
+def filters_for_fingerings(request):
+    puth = ""
+    chord = request.matchdict['name']
+    notes = musicals(chord)
+    cords_set = []
+    filters = []
+    default_filt = AllNotesNeededFilter(notes)
+    filters.append(default_filt)
+    if 'go' in request.POST:
+        if 'only_barre' in request.POST:
+            barre_filt = OnlyBarreFilter(notes)
+            filters.append(barre_filt)
+        if 'without_barre' in request.POST:
+            not_barre_filt = WithoutBarreFilter(notes)
+            filters.append(not_barre_filt)
+        if 'dist_text' in request.POST:
+            dist = request.POST['dist_text']
+            dist_filt = DistFilter(dist)
+            filters.append(dist_filt)
+        if 'first_cord' in request.POST:
+            cords_set.append(0)
+        if 'second_cord' in request.POST:
+            cords_set.append(1)
+        if 'third_cord' in request.POST:
+            cords_set.append(2)
+        if 'forth_cord' in request.POST:
+            cords_set.append(3)
+        if 'fifth_cord' in request.POST:
+            cords_set.append(4)
+        if 'sixth_cord' in request.POST:
+            cords_set.append(5)
+        if cords_set:
+            cords_filt = WithCordsFilter(cords_set)
+            filters.append(cords_filt)
+        if 'max_count_fingers' in request.POST:
+            count_fingers = request.POST['max_count_fingers']
+            fingers_filt = CountOfFingersFilter(count_fingers)
+            filters.append(fingers_filt)
+        if 'min_tune' in request.POST and 'max_tune' in request.POST:
+            min = request.POST['min_tune']
+            max = request.POST['max_tune']
+            tunes_filt = TunesFilter(min, max)
+            filters.append(tunes_filt)
+    return {
+        "notes": notes,
+        "fingerings": [",".join(map(unicode, x)) for x in iterate_fingerings(notes, filters)],
+        "filters": filters,
+         "puth": puth
+    }
 
     conn_err_msg = """
 Pyramid is having a problem using your SQL database.  The problem
