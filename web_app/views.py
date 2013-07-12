dc0d391d2416276409a76d25b855472aa2c8f7c5
kochev <kochev.alexey@gmail.com>
2013-07-12 11:09:12 +0600

    рабочие регистрация и логин!

--- web_app/views.py
+++ web_app/views.py
@@ -64,7 +64,6 @@ def get_current_user(request):
 @view_config(route_name='login', renderer='templates/login.jinja2')
 def login_view(request):
     nxt = request.params.get('next') or request.route_url('home')
-    email = ''
     did_fail = False
     if 'email' in request.POST:
         #LOGIN PROCESSING
@@ -95,10 +94,20 @@ def favicon_view(request):
 
 @view_config(route_name='registration', renderer='templates/registration.jinja2')
 def registration_view(request):
-    if "name" and "email" and "password" in request.POST:
-        if register(request.POST['name'], request.POST['email'], request.POST['password']):
-            return {'status': "зарегистрированы!".decode('utf-8')}
-    return {}
+    nxt = request.params.get('next') or request.route_url('home')
+    did_fail = False
+    if 'email' in request.POST:
+        #LOGIN PROCESSING
+        if register(request.POST["name"], request.POST["email"], request.POST["password"]):
+            headers = remember(request, id)
+            return HTTPFound(location=nxt, headers=headers)
+        else:
+            did_fail = True
+    return {
+        'login': "",
+        'next': nxt,
+        'failed_attempt': did_fail,
+        }
 
 
 @view_config(route_name='add', renderer='templates/add.jinja2')
