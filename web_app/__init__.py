c4f5fb653e3dc44f334646549700c40ea799d39a
kochev <kochev.alexey@gmail.com>
2013-07-12 14:16:15 +0600

    рабочие регистрация и логин!

--- web_app/__init__.py
+++ web_app/__init__.py
@@ -19,6 +19,7 @@ def main(global_config, **settings):
     engine = engine_from_config(settings, 'sqlalchemy.')
     DBSession.configure(bind=engine)
     Base.metadata.bind = engine
+    Base.metadata.create_all(engine)
     config = Configurator(
         settings=settings,
         authentication_policy=SessionAuthenticationPolicy())
